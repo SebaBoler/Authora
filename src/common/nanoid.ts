7cbdfb13533f27a2555838fb4fcde4ee9b9d095b
Sebastian Pietrzak <sebastian.pietrzak@giraffestudioapps.com>
2024-07-03 16:31:59 +0200

    change module to NodeNext

--- src/common/nanoid.ts
+++ src/common/nanoid.ts
@@ -1,6 +1,18 @@
-import { customAlphabet } from 'nanoid';
+// import { customAlphabet } from 'nanoid';
 
-export const nanoid = customAlphabet(
-  '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-',
-  16,
-);
+let nanoid;
+
+(async () => {
+  const nanoidModule = await import('nanoid');
+  nanoid = nanoidModule.customAlphabet(
+    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-',
+    16,
+  );
+})();
+
+export { nanoid };
+
+// export const nanoid = customAlphabet(
+//   '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-',
+//   16,
+// );
